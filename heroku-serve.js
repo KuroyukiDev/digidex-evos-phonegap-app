262f0acccc4d0c8b8fd3adae1297d6ec24ea23fc
KuroyukiDev <kuroyuki.dev@gmail.com>
2018-08-22 16:43:42 -0700

    Test Update Add background image to content area of app pages

--- heroku-serve.js
+++ heroku-serve.js
@@ -1,17 +1,18 @@
-var liveServer = require("live-server");
-const port = process.env.PORT || 8080;
-const host = process.env.IP || "127.0.0.1";
+const path = require('path');
+const bodyParser = require('body-parser');
+const express = require('express');
 
-var params = {
-    port: port, // Set the server port. Defaults to 8080.
-    host: host, // Set the address to bind to. Defaults to 0.0.0.0 or process.env.IP.
-    root: "/www", // Set root directory that's being served. Defaults to cwd.
-    open: false, // When false, it won't load your browser by default.
-//    ignore: 'scss,my/templates', // comma-separated string for paths to ignore
-    file: "index.html", // When set, serve this file for every 404 (useful for single-page applications)
-    wait: 0, // Waits for all changes, before reloading. Defaults to 0 sec.
-    mount: [['/', './']], // Mount a directory to a route.
-    logLevel: 2, // 0 = errors only, 1 = some, 2 = lots
-    middleware: [function(req, res, next) { next(); }] // Takes an array of Connect-compatible middleware that are injected into the server middleware stack
-};
-liveServer.start(params);
+const publicPath = path.join(__dirname, './www');
+const port = process.env.PORT || 3000;
+var app = express();
+
+app.use(express.static(publicPath));
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+	res.send('index.html');
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on PORT ${port}`);
+});
